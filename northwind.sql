c1fe17af7a03fb03f3fbc7226ab25fe7ba9dc769
portfoliosergiobi <148883570+portfoliosergiobi@users.noreply.github.com>
2023-10-27 12:33:23 +0200

    Add files via upload

---
+++ northwind.sql
@@ -0,0 +1,33 @@
+--Top 5 de los Productos más caros
+SELECT TOP 5 ProductName, UnitPrice
+FROM Products
+ORDER BY UnitPrice DESC
+
+--Promedio de precios de los productos por categoría
+SELECT CategoryName, AVG(UnitPrice) AS Precio_Promedio
+FROM Categories
+JOIN Products ON Categories.CategoryID = Products.CategoryID
+GROUP BY CategoryName;
+
+--Clientes que han realizado más compras
+SELECT Customers.CustomerID, Customers.ContactName, COUNT(Orders.OrderID) AS Total_Pedidos
+FROM Customers
+JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+GROUP BY Customers.CustomerID, Customers.ContactName
+ORDER BY Total_Pedidos DESC;
+
+--Empleados que han manejado más pedidos
+SELECT Employees.EmployeeID, Employees.FirstName, Employees.LastName, COUNT(Orders.OrderID) AS Total_Pedidos
+FROM Employees
+JOIN Orders ON Employees.EmployeeID = Orders.EmployeeID
+GROUP BY Employees.EmployeeID, Employees.FirstName, Employees.LastName
+ORDER BY Total_Pedidos DESC;
+
+--Empleados que han manejado pedidos por más de 10 clientes distintos.
+SELECT Employees.EmployeeID, Employees.FirstName, Employees.LastName, COUNT(DISTINCT Customers.CustomerID) AS Cliente_Unico
+FROM Employees
+JOIN Orders ON Employees.EmployeeID = Orders.EmployeeID
+JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+GROUP BY Employees.EmployeeID, Employees.FirstName, Employees.LastName
+HAVING COUNT(DISTINCT Customers.CustomerID) > 10;
+
